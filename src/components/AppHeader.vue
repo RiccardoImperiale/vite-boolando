2b257532f26cc5d878b5ac88923f908d7bc2de7c
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-21 14:44:17 +0100

    add navbar links and navbar icons dinamically

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,37 @@
 <script>
 
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data() {
+    return {
+      navLinks: [
+        { href: '#', text: 'Donna' },
+        { href: '#', text: 'Uomo' },
+        { href: '#', text: 'Bambino' }
+      ],
+      footerIcons: [
+        { href: '#', class: 'fa-regular fa-user' },
+        { href: '#', class: 'fa-regular fa-heart' },
+        { href: '#', class: 'fa-solid fa-bag-shopping' }
+      ]
+    }
+  }
 }
-</script>
 
+</script>
 <template>
   <header>
     <!-- Navbar -->
     <nav class="container">
       <!-- Nav Links  -->
       <ul class="nav_links">
-        <li><a href="#">Donna</a></li>
-        <li><a href="#">Uomo</a></li>
-        <li><a href="#">Bambini</a></li>
+        <li v-for="link in navLinks"><a :href=link.href>{{ link.text }}</a></li>
       </ul>
       <!-- Logo -->
       <img width="135" class="logo_boolando" src="/img/boolean-logo.png" alt="logo boolando">
       <!-- Nav Icons -->
       <div class="nav_icons">
-        <a href="#"><i class="fa-regular fa-user"></i></a>
-        <a href="#"><i class="fa-regular fa-heart"></i></a>
-        <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+        <a v-for="icon in footerIcons" :href=icon.href><i :class=icon.class></i></a>
       </div>
     </nav>
   </header>
