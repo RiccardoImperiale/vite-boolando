a4a0f0cb397e864443674a38012e26570cb5ab78
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-22 11:06:07 +0100

    add product card component and displaying it dynamically using props

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,12 @@
 <script>
+import ProductCard from './ProductCard.vue';
+
 export default {
     name: 'AppMain',
+    props: { product: Object },
+    components: {
+        ProductCard
+    },
     data() {
         return {
             products: [
@@ -97,31 +103,8 @@ export default {
 
 <template>
     <main>
-        <!-- Cards -->
         <div class="container cards_container">
-            <!-- First Card  -->
-            <div v-for="(product, index) in products" class="card" :key="index">
-                <div class="card_image">
-                    <img @mouseover="product.imageHover = true" @mouseleave="product.imageHover = false"
-                        :src="product.imageHover ? product.srcOnHover : product.src" :alt="product.name">
-                    <div class="heart">&hearts;</div>
-                    <div class="promo_badges">
-                        <div v-if="'sale' in product.badges" class="badge percentage">
-                            {{ product.badges.sale }}
-                        </div>
-                        <div v-show="product.badges.sustainable" class="badge sustainability">Sostenibilità</div>
-                    </div>
-                </div>
-                <div class="card_text">
-                    <h5 class="brand">{{ product.brand }}</h5>
-                    <h4>{{ product.name }}</h4>
-                    <div class="price">
-                        <span class="new_price">{{ product.price.newPrice }} &euro;</span>
-                        <span v-if="'oldPrice' in product.price" class="old_price">{{ product.price.oldPrice }}
-                            &euro;</span>
-                    </div>
-                </div>
-            </div>
+            <ProductCard :product="product" v-for="product in products" />
         </div>
     </main>
 </template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'ProductCard',
+    props: ['product'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card_image">
+            <img @mouseover="product.imageHover = true" @mouseleave="product.imageHover = false"
+                :src="product.imageHover ? product.srcOnHover : product.src" :alt="product.name">
+            <div class="heart">&hearts;</div>
+            <div class="promo_badges">
+                <div v-if="'sale' in product.badges" class="badge percentage">
+                    {{ product.badges.sale }}
+                </div>
+                <div v-show="product.badges.sustainable" class="badge sustainability">Sostenibilità</div>
+            </div>
+        </div>
+        <div class="card_text">
+            <h5 class="brand">{{ product.brand }}</h5>
+            <h4>{{ product.name }}</h4>
+            <div class="price">
+                <span class="new_price">{{ product.price.newPrice }} &euro;</span>
+                <span v-if="'oldPrice' in product.price" class="old_price">{{ product.price.oldPrice }}
+                    &euro;</span>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style></style>
