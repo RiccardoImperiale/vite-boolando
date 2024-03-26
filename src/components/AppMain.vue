0a46b42e0b8baac7c336854cbddae71a3fe17dbb
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-26 13:57:23 +0100

    change prop name for modal component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -36,43 +36,7 @@ export default {
                 @show-product="showProduct" />
         </div>
 
-        <!-- Modal  -->
-        <ProductModal :productModal="productModal" v-if="isModalOpen" @close-modal="isModalOpen = false" />
-        <!-- <div v-if="isModalOpen" class="modal">
-            <div class="card">
-                <div class="card_header">
-                    <i @click="isModalOpen = false" class="fa-solid fa-circle-xmark"></i>
-                </div>
-                <div class="card_main">
-
-                    <div class="card_left">
-                        <div class="card_image">
-                            <img :src="'./img/' + productModal.frontImage" alt="">
-                        </div>
-
-                    </div>
-                    <div class="card_right">
-                        <div class="card_text">
-                            <h5 class="brand">{{ productModal.brand }}</h5>
-                            <h4>{{ productModal.name }}</h4>
-                            <div class="price">
-                                <span v-if="productModal.badges.some(badge => badge.type === 'discount')"
-                                    class="new_price">
-                                    {{ store.getDiscountedPrice(productModal.price, productModal.badges.find(badge =>
-                badge.type === 'discount').value) }}&euro;
-                                </span>
-                                <span v-else class="new_price">{{ productModal.price }}&euro;</span>
-                                <span v-if="productModal.badges.find(badge => badge.type === 'discount')"
-                                    class="old_price">
-                                    {{ productModal.price }}&euro;
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div> -->
+        <ProductModal :product="productModal" v-if="isModalOpen" @close-modal="isModalOpen = false" />
     </main>
 </template>
 
--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -3,7 +3,7 @@ import { store } from '../store.js'
 
 export default {
     name: 'ProductModal',
-    props: { productModal: Object },
+    props: { product: Object },
     data() {
         return {
             store,
@@ -22,29 +22,28 @@ export default {
 
                 <div class="card_left">
                     <div class="card_image">
-                        <img :src="'./img/' + productModal.frontImage" alt="">
+                        <img :src="'./img/' + product.frontImage" alt="">
                     </div>
 
                 </div>
 
                 <div class="card_right">
                     <div class="card_text">
-                        <h5 class="brand">{{ productModal.brand }}</h5>
-                        <h4>{{ productModal.name }}</h4>
+                        <h5 class="brand">{{ product.brand }}</h5>
+                        <h4>{{ product.name }}</h4>
                         <div class="price">
-                            <span v-if="productModal.badges.some(badge => badge.type === 'discount')" class="new_price">
-                                {{ store.getDiscountedPrice(productModal.price, productModal.badges.find(badge =>
+                            <span v-if="product.badges.some(badge => badge.type === 'discount')" class="new_price">
+                                {{ store.getDiscountedPrice(product.price, product.badges.find(badge =>
                     badge.type === 'discount').value) }}&euro;
                             </span>
-                            <span v-else class="new_price">{{ productModal.price }}&euro;</span>
-                            <span v-if="productModal.badges.find(badge => badge.type === 'discount')" class="old_price">
-                                {{ productModal.price }}&euro;
+                            <span v-else class="new_price">{{ product.price }}&euro;</span>
+                            <span v-if="product.badges.find(badge => badge.type === 'discount')" class="old_price">
+                                {{ product.price }}&euro;
                             </span>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
