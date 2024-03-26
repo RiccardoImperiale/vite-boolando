6e0b16dc6befc266c883a70f1f8a0d03e050ceba
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-26 14:38:23 +0100

    add promo badges

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -20,19 +20,22 @@ export default {
                 <i @click="$emit('closeModal')" class="fa-solid fa-circle-xmark"></i>
             </div>
             <div class="card_main">
-
                 <div class="card_left">
                     <div class="card_image">
                         <img v-if="frontImage" :src="'./img/' + product.frontImage" alt="">
                         <img v-else :src="'./img/' + product.backImage" alt="">
+                        <div class="promo_badges">
+                            <div v-for="badge in product.badges.slice().reverse()"
+                                :class="badge.type === 'discount' ? 'percentage' : 'sustainability'" class="badge">
+                                {{ badge.value }}
+                            </div>
+                        </div>
                     </div>
                     <div class="buttons">
                         <i @click="frontImage = !frontImage" class="prev fa-solid fa-circle-chevron-left"></i>
                         <i @click="frontImage = !frontImage" class="next fa-solid fa-circle-chevron-right"></i>
                     </div>
-
                 </div>
-
                 <div class="card_right">
                     <div class="card_text">
                         <h5 class="brand">{{ product.brand }}</h5>
